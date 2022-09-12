bb1e80ecac24174fc701abbf87cd4f32c0febd36
fongradnastya <fongradnastya@gmail.com>
2022-09-12 20:47:50 +0700

    Fixed classes bugs

--- main.py
+++ main.py
@@ -3,32 +3,26 @@ import random
 
 
 class DrawFractal:
-    def __init__(self, turtle_drawer, scale=2,
-                 start_position=(20, 40), view=(0, 0)):
-        self.const_value = complex(-0.1244, 0.756)
-        self.pos_x = start_position[0]
-        self.pos_y = start_position[1]
+    def __init__(self, turtle_drawer, scale=2, view=(0, 0)):
         self.scale = scale
         self.view = view
         self.drawer = turtle_drawer
 
-    def paint_pixels(self, red, green, blue):
+    def paint_pixels(self, red, green, blue, position):
         self.drawer.penup()
-        x_pixel_number = self.pos_x - self.view[0]
-        y_pixel_number = self.pos_y - self.view[1]
-        self.drawer.goto(x_pixel_number, y_pixel_number)
+        self.drawer.goto(position[0], position[1])
         self.drawer.pendown()
         self.drawer.color(red, green, blue)
         self.drawer.dot()
 
     def render(self, n_iter, fract_size=(600, 600)):
-        number = random.randint(1, 7)
+        number = random.randint(1, 6)
         const_value = fractals_const[number]
         border = min(fract_size[0], fract_size[1]) // 2
         for y in range(-border + self.view[1], border + self.view[1], 2):
             for x in range(-border + self.view[0], border + self.view[0], 2):
-                a = x / self.scale
-                b = y / self.scale
+                a = x / (border / self.scale)
+                b = y / (border / self.scale)
                 z = complex(a, b)
                 n = 0
                 for n in range(n_iter):
@@ -41,7 +35,9 @@ class DrawFractal:
                     r = (n % 3) * 32 + 90
                     g = (n % 3) * 34
                     b = (n % 4) * 36 + 90
-                self.paint_pixels(r, g, b)
+                position = (x + self.view[0],
+                            y + self.view[1])
+                self.paint_pixels(r, g, b, position)
 
 
 fractals_const = {
@@ -58,26 +54,26 @@ class Application:
     def __init__(self, window_size=(600, 600)):
         self.screen = turtle.Screen()
         self.window_size = window_size
-        self.pos = (0, 0)
-        self.screen.colormode(255)
-        self.screen.delay(0)
-        self.screen.tracer(10000)
         self.drawer = turtle.Turtle()
 
     def run(self):
         self.setup_screen()
         fractal = DrawFractal(self.drawer)
         fractal.render(100, self.window_size)
+        turtle.done()
 
     def setup_screen(self):
+        self.screen.colormode(255)
+        self.screen.delay(0)
+        self.screen.tracer(10000)
         self.screen.title("Julia set")
         self.screen.bgcolor(120, 10, 125)
         self.drawer.hideturtle()
+        turtle.hideturtle()
+        self.drawer.speed(0)
         turtle.color("white")
         turtle.write("It's loading...", align='center',
                      font=("Verdana", 25, "normal"))
-        self.drawer.speed(0)
-        turtle.done()
 
 
 if __name__ == "__main__":
